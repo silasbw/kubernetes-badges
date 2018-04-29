9677fec8c1323f5db21416a993e0d4dbf3862559
Silas Boyd-Wickizer <silasbw@gmail.com>
2018-04-28 22:09:53 -0700

    feat(initial): badges for Deployments

---
+++ README.md
@@ -0,0 +1,3 @@
+# kubernetes-badges
+
+Track Deployment status using a badge.
---
+++ index.js
@@ -0,0 +1,63 @@
+const promisify = require('util').promisify
+const badge = promisify((format, cb) => require('gh-badges')(format, (svg, err) => cb(err, svg)))
+const express = require('express')
+const http = require('http')
+const kubernetes = require('kubernetes-client')
+const terminus = require('@godaddy/terminus')
+
+const app = express()
+let client
+
+const wrap = fn => (req, res, next) => fn(req, res).catch(next)
+app.get('/ns/:namespace/deploy/:deployment', wrap(async (req, res) => {
+  let apiRes
+  if (client.apis.apps.v1) {
+    apiRes = await client.apis.apps.v1.namespaces(req.params.namespace).deployments(req.params.deployment).get()
+  } else {
+    apiRes = await client.apis.extensions.v1beta1.namespaces(req.params.namespace).deployments(req.params.deployment).get()
+  }
+
+  //
+  // Describe the Deployment by concatenating the container image paths.
+  //
+  const containers = apiRes.body.spec.template.spec.containers
+  const imageNames = containers.map(container => container.image.split('/', 1).slice(-1)[0]);
+  
+  const subject = encodeURIComponent(req.query.subject || 'deploy')
+  const status = imageNames.join(' ')
+  const format = {
+    text: [ subject, status ],
+    format: 'svg',
+    colorscheme: 'green',
+    template: 'flat'
+  }
+  const svg = await badge(format)
+
+  res.set('Content-Type', 'image/svg+xml')
+  res.status(200).send(svg).end()
+}))
+
+async function main () {
+  let config
+  try {
+    config = kubernetes.config.getInCluster()
+  } catch (err) {
+    config = kubernetes.config.fromKubeconfig()
+  }
+  client = new kubernetes.Client({ config })
+  await client.loadSpec()
+
+  const server = http.createServer(app)
+  terminus(server, {
+    healthChecks: {
+      '/healthcheck': () => Promise.resolve()
+    }
+  })
+  server.listen(8080)
+}
+
+main()
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
